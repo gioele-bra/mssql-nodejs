ab53f46dc23667b7d96a9cc2c295f58436f849d4
gioele-bra <brancaleoni.gioele@iisgalvanimi.edu.it>
2020-05-23 10:02:11 +0000

    Added ciVettGeoRequest

--- mssql-nodejs/app.js
+++ mssql-nodejs/app.js
@@ -24,3 +24,9 @@ app.get('/ci_vettore/:foglio', function (req, res) {
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
+
+app.get('/ci_geovettore/:lng/:lat/:r', function (req, res) {
+    console.log(req.params);
+    //richiamo il metodo che ottiene l'elenco dei vettori energetici
+    sqlUtils.connect(req, res, sqlUtils.ciVettGeoRequest);
+ });
